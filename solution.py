3ed1e153d9b946f033e4c60ca52b01b3ff9969cd
Alexey <55971755+Alexey-afc@users.noreply.github.com>
2023-04-08 15:20:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
-
+from statsmodels.stats.weightstats import ztest
 
 chat_id = 543286418 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.04
+    p=ztest(x,value=300,alternative = 'larger')[1]
+    return p < alpha 
