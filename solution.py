fce2e6f01e41f81f687fba04238ccb1f57753a6d
Alexey <55971755+Alexey-afc@users.noreply.github.com>
2023-04-08 16:10:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,5 +6,5 @@ chat_id = 543286418 # Ваш chat ID, не меняйте название пе�
 
 def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha = 0.04
-    p=ztest(x,value=300,alternative = 'larger')[1]
+    p=(ztest(x,value=300,alternative = 'larger')[1])/2
     return p < alpha 
