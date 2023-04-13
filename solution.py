346bcabca2b2ce512c4f07e819868b775ca67d77
Alexey <55971755+Alexey-afc@users.noreply.github.com>
2023-04-13 23:08:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.weightstats import ztest
+from scipy.stats import t
 
 chat_id = 543286418 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    alpha = 0.04
-    p=(ztest(x,value=300,alternative = 'larger')[1])/2
-    return p < alpha 
+def solution(x: np.array) -> bool:
+    alpha = 0.4
+    mu = 300
+    n = len(x)
+    x_bar = np.mean(x)
+    s = np.std(x, ddof=1)
+
+    t_stat = (x_bar - mu) / (s / np.sqrt(n))
+    p_val = 1 - t.cdf(t_stat, n-1)
+    
+    return True if p_val < alpha else False
