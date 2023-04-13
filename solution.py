de5a24cf2c6effa896e9d9773e3c9e9cb2b2b44e
Alexey <55971755+Alexey-afc@users.noreply.github.com>
2023-04-13 23:19:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import t
+from statsmodels.stats.weightstats import ztest
+import random
+
 
 chat_id = 543286418 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool:
-    alpha = 0.4
-    mu = 300
-    n = len(x)
-    x_bar = np.mean(x)
-    s = np.std(x, ddof=1)
-
-    t_stat = (x_bar - mu) / (s / np.sqrt(n))
-    p_val = 1 - t.cdf(t_stat, n-1)
-    
-    return True if p_val < alpha else False
+   if ztest(x,300)[1] < 0.04:
+        return True
+    else:
+        return False
